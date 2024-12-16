e3afa1c23019cf66b664bd6c89bf2609b8404e66
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-12-16 21:35:05 +0900

    ログイン動作用情報

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -1,26 +1,30 @@
 <?php
-
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
- use App\Models\User;
- use Carbon\Carbon;
- use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
+
 {
-    /**
-     * Run the database seeds.
+    /** 
+
+     * Run the database seeds. 
+
      */
+
     public function run(): void
+
     {
-         //
-         $user = new User();
-         $user->name = "Test User";
-         $user->email = 'test@test.com';
-         $user->email_verified_at = Carbon::now();
-         $user->password = Hash::make('password');
-         $user->save();
+
+        $user = new User();
+        $user->name = "Test User";
+        $user->email = 'test@example.com';
+        $user->email_verified_at = Carbon::now();
+        $user->password = Hash::make('friendsgate');
+        $user->save();
     }
 }

de437f44c3f841a1ec2be695e22d46fbc63914c7
yunamatsuzaki <yunamatsuzaki@icloud.com>
2024-12-16 21:51:23 +0900

    ログイン画面修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
          <div class="col-md-5">
              <h3 class="mt-3 mb-3">ログイン</h3>
              <p>テスト用メールアドレス:test@example.com</p>
-             <p>テスト用パスワード:password</p>
+             <p>テスト用パスワード:friendsgate</p>
  
              <hr>
              <form method="POST" action="{{ route('login') }}">
